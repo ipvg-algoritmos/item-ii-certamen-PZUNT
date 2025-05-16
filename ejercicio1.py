2a533d2b71692eee01924d0322df9d130d54438c
PZUNT <patricio.zuniga@virginiogomez.cl>
2025-05-16 13:30:30 +0000

    PruebaTerminada

--- ejercicio1.py
+++ ejercicio1.py
@@ -9,3 +9,9 @@
 # 4. Luego, solicita al usuario un número y llama a la función con ese valor
 
 # 👇 Aquí comienza tu código
+num = (int(input("Ingresa un numero :")))
+def mostrar_tabla(n):
+    for i in range(1,11):
+        resultados = (n*i)
+        print (f"{n} x {i} = {resultados}")
+mostrar_tabla (num)
